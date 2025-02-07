70fa3db62abb054f052807765da176052b683334
Clement Osei Asante <clementoseiasante.nss@gmail.com>
2025-02-07 17:00:16 +0000

    ui

--- src/pages/Homepage/PropertyTools.jsx
+++ src/pages/Homepage/PropertyTools.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { ChevronDown, ChevronUp } from 'lucide-react';
 
 const PropertyTools = () => {
     return (
-        <div>PropertyTools</div>
+        <section>
+            <div className="flex justify-end space-x-4">
+                <BasicSelect label=" Select Currency" options={[{ value: "GHS", label: "Ghana Cedis ₵" }, { value: "USD", label: "United States Dollars $" }]} />
+                <BasicSelect label=" Select Tool" options={[{ value: "mortgage", label: "Mortgage Calculator" }]} />
+            </div>
+        </section>
     )
 }
+
+
+
+function BasicSelect({ label, options }) {
+   
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    return (
+
+        <div className='relative min-w-[200px]'>
+            <label htmlFor='custom-select ' className='px-6 py-2 justify-between text-center flex items-center bg-white border border-gray-400 text-gray-700 cursor-pointer rounded-3xl hover:border-[orange] transition-smooth' onClick={() => setIsOpen(!isOpen)}>{label} <ChevronDown size={18} className='mt-1' /> </label>
+            {isOpen && <div className='mt-1 bg-white rounded-lg shadow-sm'>
+                {options?.map(elem => (<MenuItem value={elem.value} key={elem.value}>{elem.label}</MenuItem>))}
+            </div>}
+
+        </div>
+    );
+}
+
+
 
 export default PropertyTools
